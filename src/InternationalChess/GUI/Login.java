f3ff4fafc25f6730173f1894ed34dfbe98f85e26
nwhvlewbvuerwv <96509115+nwhvlewbvuerwv@users.noreply.github.com>
2022-05-19 22:42:56 +0800

    Michael's draft

--- src/InternationalChess/GUI/Login.java
+++ src/InternationalChess/GUI/Login.java
@@ -47,13 +47,13 @@ public class Login extends JFrame {
         add(button);
 
         button.addActionListener(e -> {
+            Game.get().show();
             /*以下3行：
              关闭原来的界面
              */
             JComponent comp = (JComponent) e.getSource();
             Window win = SwingUtilities.getWindowAncestor(comp);
             win.dispose();
-            Game.get().show();
         });
     }
 
